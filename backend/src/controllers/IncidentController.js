ce6fa78391843096d7bf0c6106079eac23281027
kauanmocelin <kauanmocelin@gmail.com>
2020-03-26 15:40:40 -0300

    Construção do front-end

--- backend/src/controllers/IncidentController.js
+++ backend/src/controllers/IncidentController.js
@@ -34,7 +34,7 @@ module.exports = {
             .where('id', id)
             .select('ong_id')
             .first();
-        if(incident.ong_id != ong_id) {
+        if(incident.ong_id !== ong_id) {
             return response.status(401).json({ error: 'Operation not permitted.' });
         }
         await connection('incidents').where('id', id).delete();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,14 +1,22 @@
 const express = require('express');
-const routes = express.Router();
 
-const OngController = require('./controllers/OngController');
+const Ongcontroller = require('./controllers/OngController');
 const IncidentController = require('./controllers/IncidentController');
 
-routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
+const ProfileController = require('./controllers/ProfileController');
+const SessionLogin = require('./controllers/SessionController');
 
-routes.get('/incidents', IncidentController.index);
+const routes = express.Router();
+
+routes.post('/sessions', SessionLogin.create);
+
+routes.get('/ongs', Ongcontroller.index);
+routes.post('/ongs', Ongcontroller.create);
+
+routes.get('/profile', ProfileController.index);
+
 routes.post('/incidents', IncidentController.create);
+routes.get('/incidents', IncidentController.index);
 routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
